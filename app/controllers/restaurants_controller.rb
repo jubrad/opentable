207afa850edf9461a2636d9e6239027ee8968df0
Susan Gleason <susanegleason@comcast.net>
2015-11-02 15:01:43 -0500

    initial commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,48 @@
+class RestaurantsController < ApplicationController
+
+def index
+	@restaurants = Restaurant.all
+end
+
+def show
+	@restaurant = Restaurant.find(params[:id])
+end
+
+def new
+	@restaurant = Restaurant.new
+end
+
+def edit
+	@restaurant = Restaurant.find(params[:id])
+end
+
+def create
+	@restaurant = Restaurant.new(restaurant_params)
+	if @restaurant.save
+		redirect_to @restaurant, notice: "Restaurant created"
+	else
+		render action: "new"
+	end
+end
+
+def update
+	@restaurant = Restaurant.find(params[:id])
+	if @restaurant.update_attributes(restaurant_params)
+		redirect_to @restaurant, notice: "Restaurant updated"
+	else
+		render action: "edit"
+	end
+end
+
+def destroy
+	@restaurant = Restaurant.find(params[:id])
+	@restaurant.destroy
+	redirect_to restaurants_url, notice: "Restaurant deleted"
+end
+
+private
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description, :phone, :weburl)
+	end		
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+restaurants.create(:name => "Bill's Place", :description => "Good eats!", :phone => "617-777-7777")
